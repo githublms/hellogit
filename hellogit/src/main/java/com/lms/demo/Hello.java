84a5668a86dacafc94d878658242837cbea83acf
lms <1825946363@qq.com>
2020-05-27 12:53:28 +0800

    第一次提交

---
+++ hellogit/src/main/java/com/lms/demo/Hello.java
@@ -0,0 +1,11 @@
+package com.lms.demo;
+
+/**
+ * @author LMS
+ * @create 2020-05-27 12:46
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("123456");
+    }
+}
